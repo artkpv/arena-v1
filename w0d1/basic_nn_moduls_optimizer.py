457601f0f23192dc67bbdafe07493dd54f3325d3
Artyom K <artyomkarpov@gmail.com>
2022-10-29 10:58:39 +0300

    save

--- w0d1/basic_nn_moduls_optimizer.py
+++ w0d1/basic_nn_moduls_optimizer.py
@@ -24,9 +24,11 @@ x_cos_sin = t.vstack(
     (t.stack([(n*x).cos() for n in range(1, NUM_FREQUENCIES+1)]),
      t.stack([(n*x).sin() for n in range(1, NUM_FREQUENCIES+1)]))).T
 
+print('x_cos_sin.shape', x_cos_sin.shape)
+
 model = t.nn.Sequential(
     t.nn.Linear(NUM_FREQUENCIES * 2, 1),
-    t.nn.Flatten()
+    t.nn.Flatten(0,1)
 )
 
 y_pred_list = []
@@ -43,11 +45,14 @@ for step in range(TOTAL_STEPS):
 
     if step % 100 == 0:
         print(f"{loss = :.2f}")
-        #coeffs_list.append()
-        #y_pred_list.append(y_pred.detach().numpy())
+        A_n = list(model.parameters())[0].detach().numpy().squeeze()[:NUM_FREQUENCIES]
+        B_n = list(model.parameters())[0].detach().numpy().squeeze()[NUM_FREQUENCIES:]
+        a_0 = list(model.parameters())[1].item()
+        y_pred_list.append(y_pred.cpu().detach().numpy())
+        coeffs_list.append([a_0, A_n.copy(), B_n.copy()])
 
     # Update weights using gradient descent (using the parameter `LEARNING_RATE`)
-    with t.no_grad():
+    with t.inference_mode():
         for p in model.parameters():
             p -= LEARNING_RATE * p.grad
     model.zero_grad()
